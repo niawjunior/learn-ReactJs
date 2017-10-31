1572a28e9a06c9f623dae0f49655bcc9ff0f529f
niawkung <niawkung2@gmail.com>
2017-10-31 17:59:45 +0700

    แก้ไข Clock Component

--- src/Clock.js
+++ src/Clock.js
@@ -1,26 +1,26 @@
 import React from 'react';
-
 export default class Clock extends React.Component {
   constructor(props) {
     super(props);
-    
-    this.state = {
-      date: new Date()
-    }
- }
- componentDidMount() {
-  setInterval(() => {this.tick()},1000)
-}
 
-tick() {
-  this.setState({date: new Date()});
-}
+    this.state = {
+      time: new Date()
+    };
+    this.tick = this.tick.bind(this);
+  }
+  componentDidMount() {
+    setInterval(()=>{this.tick(),1000})
+  }
   
+  tick() {
+    this.setState({time:new Date()})
+  }
   render() {
-    return(
-      <div>
-        <h1>{this.state.date.toLocaleTimeString()}</h1>
-      </div>
+    return (
+      <FormattedDate time={this.state.time} />
     )
   }
 }
+
+const FormattedDate = (props) => 
+  <h2> {props.time.toLocaleTimeString()}</h2>;
