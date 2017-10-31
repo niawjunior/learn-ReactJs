b3067d22fb4e0d96e09c01a2a5170d8353e8e22b
niawkung <niawkung2@gmail.com>
2017-10-31 16:37:48 +0700

    Clock Component

---
+++ src/Clock.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      date: new Date()
+    }
+ }
+ componentDidMount() {
+  setInterval(() => {this.tick()},1000)
+}
+
+tick() {
+  this.setState({date: new Date()});
+}
+  
+  render() {
+    return(
+      <div>
+        <h1>{this.state.date.toLocaleTimeString()}</h1>
+      </div>
+    )
+  }
+}
