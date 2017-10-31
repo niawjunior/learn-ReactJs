84b0579a0b956361951c5f249dcbcb2cdf0026eb
niawkung <niawkung2@gmail.com>
2017-10-31 12:29:44 +0700

    ดึงข้อมูลจาก Api

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Button,Table  } from 'antd';
+import axios from 'axios'
 
 class App extends React.Component {
   render() {
@@ -9,7 +10,10 @@ class App extends React.Component {
       <div style={{textAlign:"center"}}>
       <Clock />
       <Counter />
+    
       <Content />
+        <br/>
+      <GetApi />
       </div>
     )
   }
@@ -131,4 +135,55 @@ class TableRow extends React.Component {
     )
   }
 }
+
+
+
+
+class GetApi extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      get: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(`https://hn.algolia.com/api/v1/search`)
+      .then(res => this.setState({ get: res.data.hits }))
+      .catch(err => console.log(err))
+  }
+  render () {
+    return (
+      <table>
+        <thead>
+          <th>Title</th>
+          <th>Author</th>
+          <th>Points</th>
+        </thead>
+        <tbody>
+         {
+           this.state.get.map(( person ) => 
+          <TableApi data_table = { person }/>
+          )
+        } 
+        </tbody>
+      </table>
+
+    )
+  }
+}
+
+class TableApi extends React.Component {
+  render () {
+    return (
+      <tr>
+        <td>{this.props.data_table.title}</td>
+        <td>{this.props.data_table.author}</td>
+        <td>{this.props.data_table.points}</td>
+      </tr>
+    )
+  }
+}
+
 export default App;
